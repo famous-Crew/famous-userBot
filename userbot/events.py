f5120a8d9dde289b94b165b2f99bcf7c8d245bd2
KENZO-404 <latukolan404@gmail.com>
2021-06-26 05:46:45 +0000

    pylint: auto fixes
    Signed-off-by: KENZO-404 <latukolan404@gmail.com>

--- userbot/events.py
+++ userbot/events.py
@@ -5,18 +5,18 @@
 #
 """ Userbot module for managing events.
  One of the main components of the userbot. """
- 
+
 import sys
 from asyncio import create_subprocess_shell as asyncsubshell
 from asyncio import subprocess as asyncsub
 from time import gmtime, strftime
 from traceback import format_exc
- 
+
 from telethon import events
- 
+
 from userbot import LOGSPAMMER, bot
- 
- 
+
+
 def register(**args):
     """ Register a new event. """
     pattern = args.get('pattern', None)
@@ -27,32 +27,32 @@ def register(**args):
     trigger_on_fwd = args.get('trigger_on_fwd', False)
     disable_errors = args.get('disable_errors', False)
     insecure = args.get('insecure', False)
- 
+
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
- 
+
     if "disable_edited" in args:
         del args['disable_edited']
- 
+
     if "ignore_unsafe" in args:
         del args['ignore_unsafe']
- 
+
     if "groups_only" in args:
         del args['groups_only']
- 
+
     if "disable_errors" in args:
         del args['disable_errors']
- 
+
     if "trigger_on_fwd" in args:
         del args['trigger_on_fwd']
- 
+
     if "insecure" in args:
         del args['insecure']
- 
+
     if pattern:
         if not ignore_unsafe:
             args['pattern'] = pattern.replace('^.', unsafe_pattern, 1)
- 
+
     def decorator(func):
         async def wrapper(check):
             if check.edit_date and check.is_channel and not check.is_group:
@@ -63,24 +63,24 @@ def register(**args):
                 check.chat_id
             else:
                 pass
- 
+
             if not trigger_on_fwd and check.fwd_from:
                 return
- 
+
             if groups_only and not check.is_group:
                 await check.respond("`I don't think this is a group.`")
                 return
- 
+
             if check.via_bot_id and not insecure and check.out:
                 return
- 
+
             try:
                 await func(check)
- 
+
             # Thanks to @kandnub for this HACK.
             # Raise StopPropagation to Raise StopPropagation
             # This needed for AFK to working properly
- 
+
             except events.StopPropagation:
                 raise events.StopPropagation
             # This is a gay exception and must be passed out. So that it doesnt
@@ -88,20 +88,20 @@ def register(**args):
             except KeyboardInterrupt:
                 pass
             except BaseException:
- 
+
                 # Check if we have to disable it.
                 # If not silence the log spam on the console,
                 # with a dumb except.
- 
+
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
- 
+
                     text = "**Geez-Project ERROR**\n"
                     link = "Silahkan chat: @VckyouuBitch"
                     text += "Untuk melaporkan kesalahan"
                     text += f"tinggal teruskan pesan ini {link}.\n"
                     text += "Vcky Siap Membantu Kamu\n"
- 
+
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
                     ftext += "\nwe logged only fact of error and date,"
@@ -120,27 +120,27 @@ def register(**args):
                     ftext += "\n\nError text:\n"
                     ftext += str(sys.exc_info()[1])
                     ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
- 
+
                     command = "git log --pretty=format:\"%an: %s\" -10"
- 
+
                     ftext += "\n\n\nLast 10 commits:\n"
- 
+
                     process = await asyncsubshell(command,
                                                   stdout=asyncsub.PIPE,
                                                   stderr=asyncsub.PIPE)
                     stdout, stderr = await process.communicate()
                     result = str(stdout.decode().strip()) \
                         + str(stderr.decode().strip())
- 
+
                     ftext += result
- 
+
                     file = open("error.log", "w+")
                     file.write(ftext)
                     file.close()
- 
+
             else:
                 pass
- 
+
         if not disable_edited:
             bot.add_event_handler(wrapper, events.MessageEdited(**args))
         bot.add_event_handler(wrapper, events.NewMessage(**args))
